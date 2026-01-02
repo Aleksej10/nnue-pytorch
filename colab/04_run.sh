6915665927703c3126474ab20dff1dcb1cb34830
Aleksej <invinovic@gmail.com>
2026-01-02 13:17:05 +0100

    added checkpoints

--- colab/04_run.sh
+++ colab/04_run.sh
@@ -1,11 +1,13 @@
 !python3 train.py \
   ./data/test80-2024-01-jan-2tb7p.min-v2.v6.binpack \
   ./data/test80-2024-01-jan-2tb7p.min-v2.v6.binpack \
+  --resume_from_checkpoint ./checkpoints/last.ckpt \
   --gpus "0," \
   --threads 4 \
   --num-workers 4 \
   --batch-size 16384 \
   --random-fen-skipping 3 \
-  --features=HalfKAv2_hm_bishops^ \
+  --features=HalfKAv2_hm_bishops \
   --lambda=1.0 \
+  --network-save-period 2 \
   --max_epochs=400
